5d40979e2edf08d87b8af6780d0ab54c6ecb55f6
jmarlin <jmarlin@coolwind.biz>
2015-02-18 17:24:19 -0500

    More fixes

--- P5OSPPB/FS/ramfs.h
+++ P5OSPPB/FS/ramfs.h
@@ -6,16 +6,16 @@
 
 #define FS_RAMFS 0
 
-void* ramfs_dir_list(block_dev* dev, void* dir, void* buf);
-void* ramfs_file_list(block_dev* dev, void* dir, void* buf);
-void* ramfs_dir_del(block_dev* dev, void* dir, void* code);
-void* ramfs_dir_add(block_dev* dev, void* dir, void* code);
-void* ramfs_file_del(block_dev* dev, void* dir, void* code);
-void* ramfs_file_add(block_dev* dev, void* dir, void* code);
-void* ramfs_file_open(block_dev* dev, void* dir, void* file);
-void* ramfs_file_close(block_dev* dev, void* file, void* code);
-void* ramfs_file_writeb(block_dev* dev, void* file, void* data, void* code);
-void* ramfs_file_readb(block_dev* dev, void* file, void* data);
+void ramfs_dir_list(block_dev* dev, void* dir, void* buf);
+void ramfs_file_list(block_dev* dev, void* dir, void* buf);
+void ramfs_dir_del(block_dev* dev, void* dir, void* code);
+void ramfs_dir_add(block_dev* dev, void* dir, void* code);
+void ramfs_file_del(block_dev* dev, void* dir, void* code);
+void ramfs_file_add(block_dev* dev, void* dir, void* code);
+void ramfs_file_open(block_dev* dev, void* dir, void* file);
+void ramfs_file_close(block_dev* dev, void* file, void* code);
+void ramfs_file_writeb(block_dev* dev, void* file, void* data, void* code);
+void ramfs_file_readb(block_dev* dev, void* file, void* data);
 
 fsdriver fs_ramfs = {
     FS_RAMFS,
