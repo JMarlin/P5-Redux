73d1cf7d4bd892bfc543fe2f4947943397a29d14
jmarlin <jmarlin@coolwind.biz>
2015-02-24 15:20:54 -0500

    Doing a test print with an empty string

--- P5OSPPB/MEMORY/PAGING.C
+++ P5OSPPB/MEMORY/PAGING.C
@@ -54,11 +54,12 @@ void map_pages(unsigned int physBase, unsigned int virtBase, unsigned int size, 
            
     for( ; i < max; i++, physBase += 0x1000) {   
         
-        DEBUG("Mapping virt 0x");
+        //DEBUG("Mapping virt 0x");
         //DEBUG_HD(i << 12);
         //DEBUG(" -> phys 0x");
         //DEBUG_HD(physBase);
         //DEBUG("\n");
+        prints("\0");
         
         //Make sure to preserve the page-reserved bit
         pageTable[i] &= 0x00000E00;
