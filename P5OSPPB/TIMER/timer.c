3fa8b7fa9904189e654c9f3b880e60b8fa58359d
jmarlin <jmarlin@coolwind.biz>
2015-02-26 09:38:52 -0500

    Making sure my timer divisor calculation is working correctly

--- P5OSPPB/TIMER/timer.c
+++ P5OSPPB/TIMER/timer.c
@@ -78,7 +78,9 @@ void init_time_chip(unsigned int freq) {
     unsigned short reload = (unsigned short)((1193182 / freq) & 0xFFFF);
     
     //Channel 0 mode 2
-    outb(TIMER_COMMAND, 0x36);
+    outb(TIMER_COMMAND, 0x34);
+    
+    prints("Setting up timer with divisor 0x"); printHexWord(reload); prints("\n")
     
     //Load reload count
     outb(TIMER0_DATA, (unsigned char)(reload & 0xFF));
