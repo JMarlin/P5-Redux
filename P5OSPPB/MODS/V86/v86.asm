97130e1ffe1d17ed5f3b2ea28e8194dd9101faad
jmarlin <jmarlin@coolwind.biz>
2015-03-03 12:13:00 -0500

    Updated v86 program to change the message to 0xDEAD should the VESA command fail

--- P5OSPPB/MODS/V86/v86.asm
+++ P5OSPPB/MODS/V86/v86.asm
@@ -4,13 +4,23 @@
 ;This is just to see if our V86 machine plays nice with 
 ;our other processes
 
+jmp start
+
+_msg: dw 0xBEEF
+
 ;Random test to see if calling a VESA interrupt works
-mov ax, ds
-mov es, ax
-mov ax, 0x1000
-mov si, ax
-mov ax, 0x4F00
-int 0x10
+start:
+    mov ax, ds
+    mov es, ax
+    mov ax, 0x1000
+    mov si, ax
+    mov ax, 0x4F00
+    int 0x10
+    cmp ax, 0x004F
+    jne wait_msg
+
+    mov ax, 0xDEAD
+    mov [_msg], ax
 
 wait_msg: 
     mov ax, 0x2
@@ -24,6 +34,6 @@ send_msg:
     ;Don't need to set bx because it should still
     ;contain the value set by the get message call
     mov cx, 0x1 ;This command don't mean shit
-    mov dx, 0xBEEF
+    mov dx, [_msg]
     int 0xFF
     jmp wait_msg
