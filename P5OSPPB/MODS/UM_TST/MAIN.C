18532b4451cc2502e6b2f9efba303bb51d180d5b
jmarlin <jmarlin@coolwind.biz>
2015-03-06 15:53:04 -0500

    Added a missing wait for enter key

--- P5OSPPB/MODS/UM_TST/MAIN.C
+++ P5OSPPB/MODS/UM_TST/MAIN.C
@@ -260,6 +260,7 @@ unsigned short getMode(void) {
     prints("    Memsize: "); printHexWord(vinfo->memSize); pchar('\n');
     prints("    Reserved: "); printHexByte(vinfo->reserved); pchar('\n');
     prints("Press enter to continue.\n");
+    scans(5, inbuf);
     modeList = (unsigned short*)((((unsigned int)vinfo->modePtrSeg) << 4) + vinfo->modePtrOff);
     prints("Available mode numbers: (from address 0x\n"); printHexDword((unsigned int)modeList); pchar('\n');
     i = 0;
