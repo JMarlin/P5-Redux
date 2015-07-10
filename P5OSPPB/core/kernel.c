37c03da8419025a3de5254a663d220c56ac25452
jmarlin <j.marlin@outlook.com>
2015-07-10 17:02:39 -0400

    #19 #41 Reintroduced (dead) continue kernel startup code

--- P5OSPPB/core/kernel.c
+++ P5OSPPB/core/kernel.c
@@ -22,7 +22,6 @@ extern char _imagename;
 
 void kernel_finish_startup(void);
 
-
 int main(void) {
 
     __asm__ ("cli");
@@ -44,6 +43,75 @@ int main(void) {
     while(1);
 }
 
+void kernel_finish_startup(void) {
+
+    unsigned int i, doffset, *sizes;
+    unsigned char *dcount;
+    context* ctx;
+    block_dev* ram0;
+    int tempCh = 0;
+    int key_stat;
+
+    DEBUG("Done.\nSetting up keyboard...");
+
+    if((key_stat = keyboard_init()) != 1) {
+        DEBUG("Failed (");
+        DEBUG_HB((unsigned char)(key_stat & 0xFF));
+        DEBUG(")\n[P5]: No input device availible.\n");
+    } else {
+        DEBUG("Done.\n");
+    }
+
+    prints("WELCOME TO P5\n");
+    prints("Please press enter to detect your keyboard type...");
+    setupKeyTable();
+    while(!(tempCh = getch()));
+    if(tempCh == 'A')
+        setupKeyTable_set1();
+
+    pchar('\n');
+    dcount = (unsigned char*)((char*)0x100000+_pkgoffset);
+    sizes = (unsigned int*)((char*)0x100001+_pkgoffset);
+
+    if(!dcount) {
+
+        prints("No modules found.\n");
+    } else {
+
+        doffset = 5 + ((dcount[0]-1)*4);
+
+        for(i = 0; i < dcount[0]; i++) {
+
+            doffset += sizes[i];
+        }
+    }
+
+    //Print kernel size and version
+    DEBUG("Image: ");
+    DEBUG(&_imagename);
+    DEBUG("\nSize: ");
+    DEBUG_HD(_pkgoffset);
+    DEBUG("b\n");
+    DEBUG("Initializing filesystem\n");
+    fs_init();
+
+    //create a ramdisk device from the extents of the kernel payload
+    //then install its fs driver and finally mount the ramdisk on root
+    DEBUG("Calculating offset to ramdisk\n");
+    doffset = 0x100005 + _pkgoffset;
+    DEBUG("Creating new ramdisk block device ram0...");
+    ram0 = blk_ram_new(doffset, sizes[0]);
+    DEBUG("Done\nInstalling ramfs filesystem driver...");
+    fs_install_driver(get_ramfs_driver());
+    DEBUG("Done\nAttaching ramfs filesystem on ram0...");
+    fs_attach(FS_RAMFS, ram0, ":");
+
+    //Start the registrar and thereby the rest of the OS
+    enterProc(exec_process(":registrar.mod", 1));
+
+    prints("PANIC: Registrar could not be started.\n");
+    while(1);
+}
 
 //Start usr prompt
 //NEEDS TO BE PURGED FROM THE CODEBASE
--- P5OSPPB/rollp5.sh
+++ P5OSPPB/rollp5.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#check to see if the user entered a commit message and, if so,
+#do a commit to git right before building (just to have better
+#build documentation)
+if (( $# > 0 )) ; then
+    git add --all :/
+    git commit -m "$1"
+    git push
+fi
+
 cd mods
 ./buildm.sh
 cd ..
