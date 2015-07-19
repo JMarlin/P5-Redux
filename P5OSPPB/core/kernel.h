1c369db82a6ac7b915ed4c2467552d852b12c3b5
JMarlin <j.marlin@outlook.com>
2015-07-19 10:45:49 -0400

    #19 #41 timer on turned v86 flag check back on and added it to kernel return

--- P5OSPPB/core/kernel.h
+++ P5OSPPB/core/kernel.h
@@ -1,7 +1,7 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-#define P5_BUILD_NUMBER 284
+#define P5_BUILD_NUMBER 288
 
 void sys_console(void);
 
--- P5OSPPB/process/process.s
+++ P5OSPPB/process/process.s
@@ -54,6 +54,7 @@ _prc_is_super: .byte 0x0
 _prc_is_v86: .byte 0x0
 _super_esp: .int 0x0
 
+.globl _switchToKernel
 _switchToKernel:
 
     /* Make sure the ESP gets restored regardless */
@@ -156,14 +157,14 @@ _switchToKernel:
  notsuper_cont:
     /* check to see if we have anything else on the stack */
     /* decided to use a flag from the last entered proc as if we're entering
-       from an IRQ handler we don't nessicarily get the eflags properly */
+       from an IRQ handler we don't nessicarily get the eflags properly 
     mov _old_eflags, %eax
     and $0x20000, %eax
     cmp $0x20000, %eax
-    /*
+    */
     mov _prc_is_v86, %eax
     cmp $0x0, %eax
-    */
+    
     jne k_continue
 
  v86_enter:
@@ -191,10 +192,13 @@ _switchToKernel:
 
 
 _returnToProc:
-
+    /*
     mov _old_eflags, %eax
     and $0x20000, %eax
     cmp $0x20000, %eax
+    */
+    mov _prc_is_v86, %eax
+    cmp $0x0, %eax
     jne user
 
     xor %esp, %esp
