9c9e2c890fa7c860375aa94662607eb55936164f
JMarlin <j.marlin@outlook.com>
2015-07-25 12:12:04 -0400

    #19 #41 #43 making sure to explicitly specify long jump syntax

--- P5OSPPB/process/process.s
+++ P5OSPPB/process/process.s
@@ -171,7 +171,7 @@ _switchToKernel:
     mov %ax, %fs
     mov %ax, %ds
     mov %ax, %es
-    jmp $0x8,the_end /* make extra sure we have the right CS */
+    jmpl $0x8,the_end /* make extra sure we have the right CS */
     the_end: call kernelEntry
 
 
