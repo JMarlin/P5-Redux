dd43b10f4cfe2a2da396db994e155baaf9df240b
jmarlin <j.marlin@outlook.com>
2015-09-24 18:15:30 -0400

    #44 fixed having written decimal addresses instead of hex like a dumbass

--- P5OSPPB/mods/uhci/main.c
+++ P5OSPPB/mods/uhci/main.c
@@ -154,10 +154,10 @@ void main(void) {
             prints("Disabling ports\n");
             //Disable the controller and its ports
             outw(usb_base, inw(usb_base) & 0xFFFE); //Set run/stop to stop
-            outw(usb_base + 10, 0x000A); //Disable port 1
-            while(inw(usb_base + 10) & 0x0004); //Wait for the port to be disabled
-            outw(usb_base + 12, 0x000A); //Disable port 2
-            while(inw(usb_base + 12) & 0x0004); //Wait for the port to be disabled
+            outw(usb_base + 0x10, 0x000A); //Disable port 1
+            while(inw(usb_base + 0x10) & 0x0004); //Wait for the port to be disabled
+            outw(usb_base + 0x12, 0x000A); //Disable port 2
+            while(inw(usb_base + 0x12) & 0x0004); //Wait for the port to be disabled
 
             prints("Setting controller defaults\n");
             //Set up default controller state (no interrupts, debug on, )
@@ -169,24 +169,24 @@ void main(void) {
 
             //Enable port one, check to see if a device is installed
             prints("Enabling port 1 and checking for device\n");
-            outw(usb_base + 10, 0x0004); //Enable port 1
-            while(!(inw(usb_base + 10) & 0x0004)); //Wait for the port to be enabled
+            outw(usb_base + 0x10, 0x0004); //Enable port 1
+            while(!(inw(usb_base + 0x10) & 0x0004)); //Wait for the port to be enabled
 
             //If device is installed, send a reset to the port
             prints("Resetting device on port 1\n");
             //Improve the timing later when we create a timer system
-            outw(usb_base + 10, 0x0204); //Port enabled, RESET state asserted
+            outw(usb_base + 0x10, 0x0204); //Port enabled, RESET state asserted
             j = 0;               //Wait
             while(j < 0xFFFFFF)
                 j = j + 1;
-            outw(usb_base + 10, 0x0004); //Port enabled, RESET state cleared
-            inw(usb_base + 10); //Read and ignore the fact that the port is now enabled
+            outw(usb_base + 0x10, 0x0004); //Port enabled, RESET state cleared
+            inw(usb_base + 0x10); //Read and ignore the fact that the port is now enabled
 
             //Wait for device to become connected ( or time out )
             j = 0;
             while(j < 0xFFFFFFF) {
 
-                if(inw(usb_base + 10) & 0x0001)
+                if(inw(usb_base + 0x10) & 0x0001)
                     break;
 
                 j = j + 1;
@@ -194,9 +194,9 @@ void main(void) {
 
 
             prints("Port status: ");
-            printHexWord(inw(usb_base + 10));
+            printHexWord(inw(usb_base + 0x10));
             pchar('/');
-            printHexWord(inw(usb_base + 10) & 0x0001);
+            printHexWord(inw(usb_base + 0x10) & 0x0001);
             pchar('\n');
             if(j < 0xFFFFFFF) {
 
@@ -229,33 +229,33 @@ void main(void) {
 
             //Enable port two, check to see if a device is installed
             prints("Enabling port 2 and checking for device\n");
-            outw(usb_base + 12, 0x0004); //Enable port 1
-            while(!(inw(usb_base + 12) & 0x0004)); //Wait for the port to be enabled
+            outw(usb_base + 0x12, 0x0004); //Enable port 1
+            while(!(inw(usb_base + 0x12) & 0x0004)); //Wait for the port to be enabled
 
             //If device is installed, send a reset to the port
             prints("Resetting device on port 2\n");
             //Improve the timing later when we create a timer system
-            outw(usb_base + 12, 0x0204); //Port enabled, RESET state asserted
+            outw(usb_base + 0x12, 0x0204); //Port enabled, RESET state asserted
             j = 0;               //Wait
             while(j < 0xFFFFFF)
                 j = j + 1;
-            outw(usb_base + 12, 0x0004); //Port enabled, RESET state cleared
-            inw(usb_base + 12); //Read and ignore the fact that the port is now enabled
+            outw(usb_base + 0x12, 0x0004); //Port enabled, RESET state cleared
+            inw(usb_base + 0x12); //Read and ignore the fact that the port is now enabled
 
             //Wait for device to become connected ( or time out )
             j = 0;
             while(j < 0xFFFFFFF) {
 
-                if(inw(usb_base + 12) & 0x0001)
+                if(inw(usb_base + 0x12) & 0x0001)
                     break;
 
                 j = j + 1;
             }
 
             prints("Port status: ");
-            printHexWord(inw(usb_base + 12));
+            printHexWord(inw(usb_base + 0x12));
             pchar('/');
-            printHexWord(inw(usb_base + 12) & 0x0001);
+            printHexWord(inw(usb_base + 0x12) & 0x0001);
             pchar('\n');
             if(j < 0xFFFFFFF) {
 
