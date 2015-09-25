2fa2062b430d7bdb89fef4ca309ad61d273ad2d4
jmarlin <j.marlin@outlook.com>
2015-09-25 14:33:10 -0400

    #44 test of not marking terminate on our frame list pointer (could equally be that it should be the last frame list pointer which wouldn't actually point to anything that should be set to terminate)

--- P5OSPPB/core/kernel.h
+++ P5OSPPB/core/kernel.h
@@ -1,7 +1,7 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-#define P5_BUILD_NUMBER 531
+#define P5_BUILD_NUMBER 532
 
 void sys_console(void);
 
--- P5OSPPB/mods/uhci/main.c
+++ P5OSPPB/mods/uhci/main.c
@@ -198,7 +198,7 @@ void main(void) {
                 //We create a control transfer to device 0 control pipe:
                 //Create a SETUP address 0 packet TD with null next pointer, insert a reference to it into the frame list
                 prints("Setting up transfer structures\n");
-                usb_ram[0] = ((unsigned int)(&usb_ram[4])) | 0x01; //First frame list pointer, points to a td at usb_ram[16] (TDs are 16-byte aligned) and has 'terminate' marked
+                usb_ram[0] = ((unsigned int)(&usb_ram[4])) | 0x00; //First frame list pointer, points to a td at usb_ram[16] (TDs are 16-byte aligned) and has 'terminate' un marked
                 usb_ram[4] = 0x1; //This starts the TD, and the first dword is the pointer to the next td. There is none, so this is marked with 'terminate'
                 usb_ram[5] = 0x1C800000; //Transfer active, Check to see later on if 0x800000 is set. If it's not, the transaction was carried out. And if 0x18000000 = 0x18000000 it had no errors
                 usb_ram[6] = 0xFFE0002D; //Empty data, enpoint 0, address 0, PID 0x2D (SETUP)
