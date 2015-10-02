8b39f05f61c935d5ce6c9b24bc8220c785ec8cf4
jmarlin <j.marlin@outlook.com>
2015-10-02 10:42:20 -0400

    #16 made init process do an idle loop for its entire lifetime

--- P5OSPPB/mods/init/main.c
+++ P5OSPPB/mods/init/main.c
@@ -21,8 +21,15 @@ void main(void) {
     prints("[init] Servers ready. Starting user process.\n");
     startProc(":usr.mod");
 
-    //And exit
-    terminate();
+    //And then spin forever and become the idle process
+    //Probably a better way to do an idle process in the
+    //future, but we need something that can always be
+    //swapped in in case all other threads are sleeping
+    //for messages, because if they're all sleeping then
+    //we'll be locked into the kernel forever looping
+    //through the scheduler trying to find a process to
+    //swap in
+    while(1);
 }
 
 
