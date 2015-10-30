cf4c9a12bdbfa420a5e078c900265396c7307aa2
jmarlin <j.marlin@outlook.com>
2015-10-30 16:55:01 -0400

    Started serious work on the WYG gui kit

--- P5OSPPB/mods/wyg/main.c
+++ P5OSPPB/mods/wyg/main.c
@@ -4,17 +4,118 @@
 #include "../include/memory.h"
 #include "../include/wyg.h"
 
+/* Windows are logically arranged as follows:
+desktop.first_child:  window_a.first_child:  button_1.first_child:  -
+       .next_sibling:         .next_sibling: window_b.first_child:  button_2.first_child:  -
+                                                     .next_sibling: -       .next_sibling: button_3.first_child:  -
+                                                                                                   .next_sibling: textbox_1.first_child:  -
+                                                                                                                           .next_sibling: -
+                                                                                                                           
+Linkage is in order of increasing z-value                                                                                                
+*/
+
 typedef struct window {
-    unsigned int flags;
+    unsigned char flags;
+    unsigned int handle;
     bitmap* context;
-    unsigned int child_count;
-    window** children;
-    
+    window* next_sibling;
+    window* parent; 
+    window* first_child;
+    unsigned int w;
+    unsigned int h;
+    unsigned int x;
+    unsigned int y;
+    unsigned char needs_redraw;
 } window;
+
+window root_window;
+unsigned int next_handle;
+unsigned int window_count;
+window** registered_windows;
+
+unsigned int newWindow(unsigned int width, unsigned int height, unsigned char flags) {
+    
+    window* new_window;
+    
+    if(!(new_window = (window*)malloc(sizeof(window))))
+        return 0;
+    
+    //This is currently BAD. If we can't realloc, it destroys the entire engine state in the process.    
+    if(!registered_windows = (window*)realloc((void*)registered_windows, sizeof(window*))))
+        return 0;
+    
+    new_window->flags = flags;
+    new_window->next_sibling = (window*)0;
+    new_window->parent = (window*)0;
+    new_window->first_child = (window*)0;
+    new_window->x = 0;
+    new_window->y = 0;
+    new_window->w = width;
+    new_window->h = height;
+    
+    //Create a drawing context for the new window
+    if(!(root_window.context = newBitmap(new_window->w, new_window->h))) {
+        
+        free((void*)new_window);
+        return 0;
+    } 
+    
+    new_window->handle = next_handle++;
+    registered_windows[window_count++] = new_window;
+    
+    return new_window->handle;
+}
+
+window* getWindowByHandle(unsigned int handle) {
+    
+    int i;
+    
+    for(i = 0; i < window_count; i++) {
+        
+        if(registered_windows[i] && registered_windows[i]->handle == handle)
+            return &registered_windows[i];
+    }
+    
+    return (window*)0;
+}
+
+void showModes(void) {
+
+
+    unsigned short mode_count;
+    unsigned short i;
+    screen_mode* mode;
+
+    prints("Enumerating modes...");
+    mode_count = enumerateModes();
+    prints("done\n");
+
+    prints("\nAvailible modes:\n");
+    for(i = 0; i < mode_count; i++) {
+
+        mode = getModeDetails(i);
+        prints("    ");
+        printDecimal((unsigned int)i);
+        prints(") ");
+        printDecimal((unsigned int)mode->width);
+        pchar('x');
+        printDecimal((unsigned int)mode->height);
+        prints(", ");
+        printDecimal((unsigned int)mode->depth);
+        prints("bpp");
+
+        if(mode->is_linear)
+            prints(" linear");
+
+        pchar('\n');
+    }
+}
 
 void main(void) {
 
     unsigned int parent_pid;
+    screen_mode* mode;
+    unsigned short num;
 
     //Get the 'here's my pid' message from init
     getMessage(&temp_msg);
@@ -33,6 +134,73 @@ void main(void) {
         terminate();
     }
 
+    if(!initGfx()) {
+        
+        prints("\n[WYG] failed to get the GFX server.\n");
+        postMessage(REGISTRAR_PID, REG_DEREGISTER, SVC_WYG);
+        postMessage(parent_pid, 0, 0); //Tell the parent we're done registering
+        terminate();
+    }
+
+    //Prompt user for a screen mode
+    showModes();
+    prints("mode: ");
+    scans(10, inbuf);
+    num = inbuf[0] > '9' ? inbuf[0] - 'A' + 10 : inbuf[0] - '0';
+
+    if(!setScreenMode(num)) {
+
+        prints("[WYG] Could not set screen mode.\n");
+        postMessage(REGISTRAR_PID, REG_DEREGISTER, SVC_WYG);
+        postMessage(parent_pid, 0, 0); //Tell the parent we're done registering
+        terminate();
+    }
+
+    if(num) {
+
+        mode = getModeDetails(num);
+        startGui(mode->width, mode->height);
+    } else {
+
+        prints("[WYG] Staying in text mode.\n");
+        postMessage(REGISTRAR_PID, REG_DEREGISTER, SVC_WYG);
+        postMessage(parent_pid, 0, 0); //Tell the parent we're done registering
+        terminate();
+    }
+    
+    if(!registered_windows = (window*)malloc(sizeof(window*)))) {
+        
+        prints("[WYG] Couldn't allocate window LUT.\n");
+        postMessage(REGISTRAR_PID, REG_DEREGISTER, SVC_WYG);
+        postMessage(parent_pid, 0, 0); //Tell the parent we're done registering
+        terminate();
+    }
+    
+    next_handle = 1;
+    root_window.handle = next_handle++;
+    registered_windows[0] = &root_window;
+    window_count = 1;
+
+    //Init the root window (aka the desktop)
+    root_window.flags = WIN_UNDECORATED | WIN_FIXEDSIZE | WIN_VISIBLE;
+    root_window.next_sibling = (window*)0;
+    root_window.parent = (window*)0;
+    root_window.first_child = (window*)0;
+    root_window.x = 0;
+    root_window.y = 0;
+    root_window.w = mode->width;
+    root_window.h = mode->height;
+    
+    //Create a drawing context for the root window
+    if(!(root_window.context = newBitmap(root_window.w, root_window.h))) {
+        
+        prints("[WYG] Could not allocate a context for the root window.\n");
+        free((void*)registered_windows);
+        postMessage(REGISTRAR_PID, REG_DEREGISTER, SVC_WYG);
+        postMessage(parent_pid, 0, 0); //Tell the parent we're done registering
+        terminate();
+    } 
+
     postMessage(parent_pid, 0, 1); //Tell the parent we're done registering
 
     //Now we can start the main message loop and begin handling
@@ -44,7 +212,15 @@ void main(void) {
         switch(temp_msg.command) {
 
             case WYG_CREATE_WINDOW:
-                postMessage(temp_msg.source, GFX_CREATE_WINDOW, (unsigned int)mode_count);
+                postMessage(temp_msg.source, WYG_CREATE_WINDOW, (unsigned int)newWindow((temp_msg.payload & 0xFFF00000) >> 20, (temp_msg.payload & 0xFFF00) >> 8, temp_msg.payload & 0xFF));
+            break;
+            
+            case WYG_GET_CONTEXT:
+                postMessage(temp_msg.source, WYG_GET_CONTEXT, (unsigned int)getWindowContext(temp_msg.payload));
+            break;
+            
+            case WYG_MOVE_WINDOW:
+                postMessage(temp_msg.source, WYG_MOVE_WINDOW, (unsigned int)getWindowContext(temp_msg.payload));
             break;
 
             default:
