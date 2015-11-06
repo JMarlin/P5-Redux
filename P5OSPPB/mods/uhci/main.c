ef2443219d7c63444f10b762c2eca3c6791c3178
jmarlin <j.marlin@outlook.com>
2015-11-06 17:48:12 -0500

    Set up get device descriptor code to attempt it twice

--- P5OSPPB/mods/uhci/main.c
+++ P5OSPPB/mods/uhci/main.c
@@ -249,140 +249,148 @@ void main(void) {
     
                     if(inw(portreg) & 0x0001) {
         
-                        //We create a control transfer to device 0 control pipe:
-                        //Create a SETUP address 0 packet TD with null next pointer, insert a reference to it into the frame list
-                        prints("Setting up transfer structures\n");
-                        for(j = 0; j < 0x400; j++)
-                            frame_list[j] = ((unsigned int)(&usb_ram[0])) | 0x02; //All frame list pointers points to a qh at usb_ram[0] (TDs are 16-byte aligned) and has 'terminate' unmarked
-                        //This is a queue head which will point to our default queue of TDs
-                        usb_ram[0] = 0x01; //This is the head of our test queue, set to terminate
-                        usb_ram[1] = ((unsigned int)&usb_ram[4] & 0xFFFFFFF0); // Pointer to the first TD in the list, 
-                        usb_ram[4] = ((unsigned int)&usb_ram[12] & 0xFFFFFFF0) | 0x4; //This starts the TD, and points to the next TD in the list -- also marked depth-first
-                        usb_ram[5] = 0x1C800000; //Transfer active, Check to see later on if 0x800000 is set. If it's not, the transaction was carried out. And if 0x18000000 = 0x18000000 it had no errors
-                        usb_ram[6] = 0x00E0002D; //eight bytes data, endpoint 0, address 0, PID 0x2D (SETUP), DATA 0
-                        usb_ram[7] = (unsigned int)&usb_ram[20]; //Pointer for the buffer that the controller should read the packet data from
-                        usb_ram[8] = 0; usb_ram[9] = 0; usb_ram[10] = 0; usb_ram[11] = 0; //There's extra shit here, fuck if I know why
-                        usb_ram[12] = 0x1; //This starts the TD and is a null terminate pointer as it will also be the last TD
-                        usb_ram[13] = 0x1C800000; //Transfer active, Check to see later on if 0x800000 is set. If it's not, the transaction was carried out. And if 0x18000000 = 0x18000000 it had no errors
-                        usb_ram[14] = 0x00E80069; //eight bytes data (we will be splitting the descriptor into multiple 8-byte packets), endpoint 0, address 0, PID 0x69 (IN), DATA 1
-                        usb_ram[15] = (unsigned int)&usb_ram[22]; //Pointer for the buffer that the controller should read the packet data to
-                        usb_ram[16] = 0; usb_ram[17] = 0; usb_ram[18] = 0; usb_ram[19] = 0; //There's extra shit here, fuck if I know why
-                        usb_buf = (unsigned char*)&usb_ram[20]; //Treating the packet data buffer as a byte array to make setting it up a little easier
-                        usb_buf[0] = 0x80; //(device, standard, device-to-host)
-                        usb_buf[1] = 0x06; //Get descriptor
-                        usb_buf[2] = 0x00; //Descriptor index 0
-                        usb_buf[3] = 0x01; //Device descriptor
-                        usb_buf[4] = 0x0; //wIndex = 0
-                        usb_buf[5] = 0x0; //wIndex = 0
-                        usb_buf[6] = 0x12; //wLength = 18 bytes //Set to 8 to match our max packet size in the setup packet
-                        usb_buf[7] = 0x00; //wLength = 18 byes
-                        //usb_ram[12] = 0x01000680; //bmRequestType = 0x80 (device, standard, device-to-host), bRequest = 0x06 (descriptor), wValue = 0x0100 (device descriptor/descriptor index 0)
-                        //usb_ram[13] = 0x00120000; //wIndex = 0x0000 (unused in this request), wLength = 0x0012 (18 bytes, which is the length of a device descriptor)
-    
-    /*
-      GetDvcDescrReq:
-    DB SRRTIn+SRRTTypeStandard+SRRTRecipDevice = 0x80 + 0x00 + 0x00 = 0x80
-    DB SRRQGetDescriptor = 0x06
-    DB 0
-    DB DescrTypeDevice = 0x01
-    DW 0
-    DW DvcDescrSize = 0x12
-    */
-    
-                        usb_ram[22] = 0x0; //This is the buffer into which our device descriptor should be read
-                        usb_ram[23] = 0x0;
-                        usb_ram[24] = 0x0;
-                        usb_ram[25] = 0x0;
-                        usb_ram[26] = 0x0;
-    
-                        while(1) {
-    
-                            //Set frame number to 0
-                            outw(usb_base + 0x06, 0x0); //Set the current frame number to 0
-    
-                            //Set run/stop to run
-                            //Make sure that none of the interrupt flags are set, and clear them if they are
-                            if(inw(usb_base + 0x02) & 0x10) {
-    
-                                prints("[uhci] INTERNAL PROCESS ERROR!");
-                                while(1);
+                        //attempt device init twice
+                        for(j = 0; j < 2; j++) {
+            
+                            //We create a control transfer to device 0 control pipe:
+                            //Create a SETUP address 0 packet TD with null next pointer, insert a reference to it into the frame list
+                            prints("Setting up transfer structures\n");
+                            for(j = 0; j < 0x400; j++)
+                                frame_list[j] = ((unsigned int)(&usb_ram[0])) | 0x02; //All frame list pointers points to a qh at usb_ram[0] (TDs are 16-byte aligned) and has 'terminate' unmarked
+                            //This is a queue head which will point to our default queue of TDs
+                            usb_ram[0] = 0x01; //This is the head of our test queue, set to terminate
+                            usb_ram[1] = ((unsigned int)&usb_ram[4] & 0xFFFFFFF0); // Pointer to the first TD in the list, 
+                            usb_ram[4] = ((unsigned int)&usb_ram[12] & 0xFFFFFFF0) | 0x4; //This starts the TD, and points to the next TD in the list -- also marked depth-first
+                            usb_ram[5] = 0x1C800000; //Transfer active, Check to see later on if 0x800000 is set. If it's not, the transaction was carried out. And if 0x18000000 = 0x18000000 it had no errors
+                            usb_ram[6] = 0x00E0002D; //eight bytes data, endpoint 0, address 0, PID 0x2D (SETUP), DATA 0
+                            usb_ram[7] = (unsigned int)&usb_ram[20]; //Pointer for the buffer that the controller should read the packet data from
+                            usb_ram[8] = 0; usb_ram[9] = 0; usb_ram[10] = 0; usb_ram[11] = 0; //There's extra shit here, fuck if I know why
+                            usb_ram[12] = 0x1; //This starts the TD and is a null terminate pointer as it will also be the last TD
+                            usb_ram[13] = 0x1C800000; //Transfer active, Check to see later on if 0x800000 is set. If it's not, the transaction was carried out. And if 0x18000000 = 0x18000000 it had no errors
+                            usb_ram[14] = 0x00E80069; //eight bytes data (we will be splitting the descriptor into multiple 8-byte packets), endpoint 0, address 0, PID 0x69 (IN), DATA 1
+                            usb_ram[15] = (unsigned int)&usb_ram[22]; //Pointer for the buffer that the controller should read the packet data to
+                            usb_ram[16] = 0; usb_ram[17] = 0; usb_ram[18] = 0; usb_ram[19] = 0; //There's extra shit here, fuck if I know why
+                            usb_buf = (unsigned char*)&usb_ram[20]; //Treating the packet data buffer as a byte array to make setting it up a little easier
+                            usb_buf[0] = 0x80; //(device, standard, device-to-host)
+                            usb_buf[1] = 0x06; //Get descriptor
+                            usb_buf[2] = 0x00; //Descriptor index 0
+                            usb_buf[3] = 0x01; //Device descriptor
+                            usb_buf[4] = 0x0; //wIndex = 0
+                            usb_buf[5] = 0x0; //wIndex = 0
+                            usb_buf[6] = 0x12; //wLength = 18 bytes //Set to 8 to match our max packet size in the setup packet
+                            usb_buf[7] = 0x00; //wLength = 18 byes
+                            //usb_ram[12] = 0x01000680; //bmRequestType = 0x80 (device, standard, device-to-host), bRequest = 0x06 (descriptor), wValue = 0x0100 (device descriptor/descriptor index 0)
+                            //usb_ram[13] = 0x00120000; //wIndex = 0x0000 (unused in this request), wLength = 0x0012 (18 bytes, which is the length of a device descriptor)
+        
+        /*
+        GetDvcDescrReq:
+        DB SRRTIn+SRRTTypeStandard+SRRTRecipDevice = 0x80 + 0x00 + 0x00 = 0x80
+        DB SRRQGetDescriptor = 0x06
+        DB 0
+        DB DescrTypeDevice = 0x01
+        DW 0
+        DW DvcDescrSize = 0x12
+        */
+        
+                            usb_ram[22] = 0x0; //This is the buffer into which our device descriptor should be read
+                            usb_ram[23] = 0x0;
+                            usb_ram[24] = 0x0;
+                            usb_ram[25] = 0x0;
+                            usb_ram[26] = 0x0;
+        
+                            while(1) {
+        
+                                //Set frame number to 0
+                                outw(usb_base + 0x06, 0x0); //Set the current frame number to 0
+        
+                                //Set run/stop to run
+                                //Make sure that none of the interrupt flags are set, and clear them if they are
+                                if(inw(usb_base + 0x02) & 0x10) {
+        
+                                    prints("[uhci] INTERNAL PROCESS ERROR!");
+                                    while(1);
+                                }
+                                
+                                //Set software debug on
+                                outw(usb_base, inw(usb_base) | 0x20);
+        
+                                //We should clear HCHalted here, to make sure we're not reading our own value later on
+                                outw(usb_base + 0x02, 0x20); //The usbsts register is R/WC which means that writing a one to a location resets its value to zero
+                                prints("Setting host controller to run...\n");
+                                outw(usb_base, inw(usb_base) | 0x0001);
+                                while(inw(usb_base + 0x02) & 0x20); //Wait until the halted status clears
+        
+                                //Poll HCHalted until the device is halted
+                                while(!(inw(usb_base + 0x02) & 0x20));
+        
+        
+                                //Check first TD (SETUP)
+                                //Display TD status
+                                prints("TD 1 (SETUP)\n");
+                                //Display the status of the transaction descriptor
+                                prints(" (");
+                                printHexDword(usb_ram[5]);
+                                prints(") USBSTS: 0x");
+                                printHexWord(inw(usb_base + 0x02));
+                                pchar('\n');
+                                if(!((usb_ram[5] & 0x800000)) && ((usb_ram[5] & 0x18000000) != 0)) {
+        
+                                    prints("    Setup completed successfully\n");
+                                    td1 = 1;
+                                }
+        
+                                if(!(usb_ram[5] & 0x18000000)) {
+        
+                                    prints("    Could not recover from transfer errors.\n");
+                                    break;
+                                }
+        
+                                //Check the second TD (IN)
+                                //Display TD status
+                                prints("TD 2 (IN)\n");
+                                //Display the status of the transaction descriptor
+                                prints(" (");
+                                printHexDword(usb_ram[13]);
+                                prints(") USBSTS: 0x");
+                                printHexWord(inw(usb_base + 0x02));
+                                pchar('\n');
+                                if(!((usb_ram[13] & 0x800000)) && ((usb_ram[13] & 0x18000000) != 0)) {
+        
+                                    prints("    Setup completed successfully\n");
+                                    printHexDword(usb_ram[22]); pchar('\n');
+                                    printHexDword(usb_ram[23]); pchar('\n');
+                                    printHexDword(usb_ram[24]); pchar('\n');
+                                    printHexDword(usb_ram[25]); pchar('\n');
+                                    printHexDword(usb_ram[26]); pchar('\n');
+                                    td2 = 1;
+                                    while(1); //REMOVE ME
+                                }
+        
+                                if(!(usb_ram[13] & 0x18000000)) {
+        
+                                    prints("    Could not recover from transfer errors.\n");
+                                    break;
+                                }
+        
+                                if(td1 && td2)
+                                    break;
+        
+                                //Create a IN address 0 packet TD with null next pointer, insert a reference to it into the frame list
+                                //Set frame number to 0
+                                //Set run/stop to run
+                                //Poll HCHalted until the device is halted
+                                //Display TD status
+                                //Display recieved data
+                                //Create an OUT address 0 packet TD with null next pointer (data length 0, STATUS PHASE), insert a reference to it into the frame list
+                                //Set frame number to 0
+                                //Set run/stop to run
+                                //Poll HCHalted until the device is halted
+                                //Display TD status
                             }
                             
-                            //Set software debug on
-                            outw(usb_base, inw(usb_base) | 0x20);
-    
-                            //We should clear HCHalted here, to make sure we're not reading our own value later on
-                            outw(usb_base + 0x02, 0x20); //The usbsts register is R/WC which means that writing a one to a location resets its value to zero
-                            prints("Setting host controller to run...\n");
-                            outw(usb_base, inw(usb_base) | 0x0001);
-                            while(inw(usb_base + 0x02) & 0x20); //Wait until the halted status clears
-    
-                            //Poll HCHalted until the device is halted
-                            while(!(inw(usb_base + 0x02) & 0x20));
-    
-    
-                            //Check first TD (SETUP)
-                            //Display TD status
-                            prints("TD 1 (SETUP)\n");
-                            //Display the status of the transaction descriptor
-                            prints(" (");
-                            printHexDword(usb_ram[5]);
-                            prints(") USBSTS: 0x");
-                            printHexWord(inw(usb_base + 0x02));
-                            pchar('\n');
-                            if(!((usb_ram[5] & 0x800000)) && ((usb_ram[5] & 0x18000000) != 0)) {
-    
-                                prints("    Setup completed successfully\n");
-                                td1 = 1;
-                            }
-    
-                            if(!(usb_ram[5] & 0x18000000)) {
-    
-                                prints("    Could not recover from transfer errors.\n");
-                                break;
-                            }
-    
-                            //Check the second TD (IN)
-                            //Display TD status
-                            prints("TD 2 (IN)\n");
-                            //Display the status of the transaction descriptor
-                            prints(" (");
-                            printHexDword(usb_ram[13]);
-                            prints(") USBSTS: 0x");
-                            printHexWord(inw(usb_base + 0x02));
-                            pchar('\n');
-                            if(!((usb_ram[13] & 0x800000)) && ((usb_ram[13] & 0x18000000) != 0)) {
-    
-                                prints("    Setup completed successfully\n");
-                                printHexDword(usb_ram[22]); pchar('\n');
-                                printHexDword(usb_ram[23]); pchar('\n');
-                                printHexDword(usb_ram[24]); pchar('\n');
-                                printHexDword(usb_ram[25]); pchar('\n');
-                                printHexDword(usb_ram[26]); pchar('\n');
-                                td2 = 1;
-                                while(1); //REMOVE ME
-                            }
-    
-                            if(!(usb_ram[13] & 0x18000000)) {
-    
-                                prints("    Could not recover from transfer errors.\n");
-                                break;
-                            }
-    
+                            //Exit early if the first attempt worked
                             if(td1 && td2)
                                 break;
-    
-                            //Create a IN address 0 packet TD with null next pointer, insert a reference to it into the frame list
-                            //Set frame number to 0
-                            //Set run/stop to run
-                            //Poll HCHalted until the device is halted
-                            //Display TD status
-                            //Display recieved data
-                            //Create an OUT address 0 packet TD with null next pointer (data length 0, STATUS PHASE), insert a reference to it into the frame list
-                            //Set frame number to 0
-                            //Set run/stop to run
-                            //Poll HCHalted until the device is halted
-                            //Display TD status
                         }
                         
                         while(1); //Temporarily hang once we're done with the first port for testing
