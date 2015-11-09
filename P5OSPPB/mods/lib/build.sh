8b88541f1bc794fd5ba1f81bebda19942b1a212f
jmarlin <j.marlin@outlook.com>
2015-11-09 10:15:05 -0500

    Updated lib build script to link memory.o and p5.o

--- P5OSPPB/mods/lib/build.sh
+++ P5OSPPB/mods/lib/build.sh
@@ -2,8 +2,7 @@
 
 gcc -c -o pci.o pci.c -nostdlib -nostdinc -ffreestanding -m32
 gcc -c -o gfx.o gfx.c -nostdlib -nostdinc -ffreestanding -m32
-gcc -c -o p5tmp.o p5.c -nostdlib -nostdinc -ffreestanding -m32
+gcc -c -o p5.o p5.c -nostdlib -nostdinc -ffreestanding -m32
 gcc -c -o memory.o memory.c -nostdlib -nostdinc -ffreestanding -m32
-ld -o p5.o p5tmp.o memory.o -melf_i386
 gcc -c -o wyg.o wyg.c -nostdlib -nostdinc -ffreestanding -m32
 as -o p5s.o p5.s --32
