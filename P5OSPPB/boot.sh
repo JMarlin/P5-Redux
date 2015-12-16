d4d132fef8687f6f1add6ce8bfec764edeb3d184
jmarlin <j.marlin@outlook.com>
2015-12-16 12:00:44 -0500

    Added standard modules back to rampack

--- P5OSPPB/boot.sh
+++ P5OSPPB/boot.sh
@@ -1,8 +1,7 @@
 cp bin/p5kern.bin ../kpkg/
 cd ../rampak/
 gcc -o rampak main.c
-#./rampak p5.rd test.txt usr.mod init.mod registrar.mod vesa.mod v86.mod idle.mod wyg.mod pci.mod uhci.mod
-./rampak p5.rd test.txt init.mod registrar.mod idle.mod key.mod
+./rampak p5.rd test.txt usr.mod init.mod registrar.mod vesa.mod v86.mod idle.mod wyg.mod pci.mod uhci.mod key.mod
 cp p5.rd ../kpkg/
 cd ../kpkg/
 gcc -o kpkg main.c
