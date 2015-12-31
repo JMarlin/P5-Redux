5d527750736756557104f73fe0d59c3bcb35df48
jmarlin <j.marlin@outlook.com>
2015-12-31 14:10:35 -0500

    Uncommented body frame drawing

--- P5OSPPB/mods/wyg/main.c
+++ P5OSPPB/mods/wyg/main.c
@@ -1096,7 +1096,7 @@ void drawFrame(window* cur_window) {
     bmpDrawPanel(cur_window->context, 3, 3, cur_window->w - 6, 22, RGB(238, 203, 137), 1, 1);
     
     //Body border
-    //bmpDrawPanel(cur_window->context, 3, 25, cur_window->w - 6, cur_window->h - 32, RGB(238, 203, 137), 1, 1);
+    bmpDrawPanel(cur_window->context, 3, 25, cur_window->w - 6, cur_window->h - 32, RGB(238, 203, 137), 1, 1);
     
     //Left frame
     //bmpFillRect(cur_window->context, 1, 1, 2, cur_window->h - 2, RGB(238, 203, 137)); 
