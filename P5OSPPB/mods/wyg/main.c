52ad0be91440630241552ea310da55f4a03725cb
jmarlin <j.marlin@outlook.com>
2016-02-04 14:41:20 -0500

    Seeing if frame drawing suddenly works

--- P5OSPPB/mods/wyg/main.c
+++ P5OSPPB/mods/wyg/main.c
@@ -1042,11 +1042,11 @@ void drawFrame(window* cur_window) {
     bmpFillRect(cur_window->context, 3, 25, cur_window->w - 6, 2, RGB(238, 203, 137)); 
     
     //Bottom frame
-    //bmpFillRect(cur_window->context, 3, cur_window->h - 20, cur_window->w - 6, 2, RGB(238, 203, 137)); 
+    bmpFillRect(cur_window->context, 3, cur_window->h - 20, cur_window->w - 6, 2, RGB(238, 203, 137)); 
         
     //Button
-    //bmpDrawPanel(cur_window->context, cur_window->w - 24, 4, 20, 20, RGB(238, 203, 137), 1, 0);
-    //bmpFillRect(cur_window->context, cur_window->w - 23, 4, 18, 18, RGB(238, 203, 137)); 
+    bmpDrawPanel(cur_window->context, cur_window->w - 24, 4, 20, 20, RGB(238, 203, 137), 1, 0);
+    bmpFillRect(cur_window->context, cur_window->w - 23, 4, 18, 18, RGB(238, 203, 137)); 
     
     drawTitlebar(cur_window, cur_window->next_sibling == (window*)0);
     
