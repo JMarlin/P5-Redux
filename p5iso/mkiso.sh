846e7bfea0bd5c036a3259b648f16852095a38bd
Joseph Marlin <jmarlin@CENTOS-SERVER.coolwind.local>
2016-02-04 11:27:44 -0500

    updated mount point of mkiso

--- p5iso/mkiso.sh
+++ p5iso/mkiso.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mount -o loop pboot_fat12.img /mnt/dos
-cp ../dosload/p5kern.bin /mnt/dos/
-umount /mnt/dos
+mount -o loop pboot_fat12.img /mnt/iso
+cp ../dosload/p5kern.bin /mnt/iso/
+umount /mnt/iso
 cp pboot_fat12.img ./cdcontent/
 mkisofs -o p5.iso -V P5OS -b pboot_fat12.img cdcontent
