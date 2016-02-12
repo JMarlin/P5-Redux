486a04818ae3671ea96cbd2e75754b2786509f9d
jmarlin <j.marlin@outlook.com>
2016-02-12 13:31:58 -0500

    Realized that it would be totally helpful if new rectangles actually took on the passed values

--- P5OSPPB/mods/wyg/rect.c
+++ P5OSPPB/mods/wyg/rect.c
@@ -10,9 +10,14 @@ void Rect_deleter(void* item) {
 Rect* Rect_new(unsigned int top, unsigned int left, unsigned int bottom, unsigned int right) {
     
     Rect* rect = (Rect*)malloc(sizeof(Rect));
-    
+    	
     if(!rect)
         return rect;
-        
+    
+	rect->top = top;
+	rect->left = left;
+	rect->bottom = bottom;
+	rect->right = right;
+	    
     return rect;
 }
