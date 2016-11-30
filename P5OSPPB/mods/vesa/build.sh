1543466d9a5d4822a61c2d2ae0a849b9bbc49438
Joseph Marlin <j.marlin@outlook.com>
2016-11-30 22:26:30 +0000

    Added debug symbols to vesa.mod build

--- P5OSPPB/mods/vesa/build.sh
+++ P5OSPPB/mods/vesa/build.sh
@@ -7,8 +7,8 @@ gcc -o imgconvert imgconvert.c
 rm font.h
 ./imgconvert > font.h
 
-as -o init.o init.s --32
-gcc -c -o main.o main.c -nostdlib -nostartfiles -nodefaultlibs -nostdinc -ffreestanding -m32
-ld -o vesa.o -T lscpt.lds init.o main.o -melf_i386 ../lib/p5.o ../lib/p5s.o
+as -o init.o init.s --32 -g
+gcc -c -o main.o main.c -nostdlib -nostartfiles -nodefaultlibs -nostdinc -ffreestanding -m32 -g
+ld -o vesa.o -T lscpt.lds init.o main.o -melf_i386 ../lib/p5.o ../lib/p5s.o -g
 objcopy -O binary -j .init -j .text -j .data -j .bss --set-section-flags .bss=alloc,load,contents vesa.o vesa.mod
 cp vesa.mod ../../../rampak/
