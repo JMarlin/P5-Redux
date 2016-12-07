e5502d35d7d243f090037f5560b117b8435e2476
Joseph Marlin <j.marlin@outlook.com>
2016-12-07 21:03:56 +0000

    Added debug symbols to app lib build

--- P5OSPPB/core/kernel.h
+++ P5OSPPB/core/kernel.h
@@ -1,7 +1,7 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-#define P5_BUILD_NUMBER 1146
+#define P5_BUILD_NUMBER 1161
 
 void sys_console(void);
 
--- P5OSPPB/mods/lib/build.sh
+++ P5OSPPB/mods/lib/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-gcc -c -o pci.o pci.c -nostdlib -nostdinc -ffreestanding -m32
-gcc -c -o gfx.o gfx.c -nostdlib -nostdinc -ffreestanding -m32
-gcc -c -o memory.o memory.c -nostdlib -nostdinc -ffreestanding -m32
-gcc -c -o p5.o p5.c -nostdlib -nostdinc -ffreestanding -m32
-gcc -c -o key.o key.c -nostdlib -nostdinc -ffreestanding -m32
-gcc -c -o mouse.o mouse.c -nostdlib -nostdinc -ffreestanding -m32
-gcc -c -o wyg.o wyg.c -nostdlib -nostdinc -ffreestanding -m32
-as -o p5s.o p5.s --32
-as -o memorys.o memory.s --32
+gcc -c -o pci.o pci.c -nostdlib -nostdinc -ffreestanding -m32 -g
+gcc -c -o gfx.o gfx.c -nostdlib -nostdinc -ffreestanding -m32 -g
+gcc -c -o memory.o memory.c -nostdlib -nostdinc -ffreestanding -m32 -g
+gcc -c -o p5.o p5.c -nostdlib -nostdinc -ffreestanding -m32 -g
+gcc -c -o key.o key.c -nostdlib -nostdinc -ffreestanding -m32 -g
+gcc -c -o mouse.o mouse.c -nostdlib -nostdinc -ffreestanding -m32 -g
+gcc -c -o wyg.o wyg.c -nostdlib -nostdinc -ffreestanding -m32 -g
+as -o p5s.o p5.s --32 -g
+as -o memorys.o memory.s --32 -g
