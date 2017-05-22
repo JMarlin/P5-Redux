766aeee27558a7630dae0e5d300485e938bd429d
Joseph Marlin <j.marlin@outlook.com>
2017-05-21 22:20:09 -0400

    removing head issue from kernel.h

--- P5OSPPB/core/kernel.h
+++ P5OSPPB/core/kernel.h
@@ -1,11 +1,7 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-<<<<<<< HEAD
-#define P5_BUILD_NUMBER 1248
-=======
 #define P5_BUILD_NUMBER 1283
->>>>>>> 724f2b8b0f53c251fe14e09dba55c91aac85839d
 
 void sys_console(void);
 
