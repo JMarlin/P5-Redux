d09406888859061d6b0378f497b107a6a9d30079
Joseph Marlin <j.marlin@outlook.com>
2017-06-09 22:21:39 -0400

    Added link reference to memorys.o in vfs.mod build script

--- P5OSPPB/mods/vfs/build.sh
+++ P5OSPPB/mods/vfs/build.sh
@@ -2,6 +2,6 @@
 
 as -o init.o init.s --32
 gcc -c -o main.o main.c -nostdlib -nostartfiles -nodefaultlibs -nostdinc -ffreestanding -m32
-ld -o vfs.o -T lscpt.lds init.o main.o -melf_i386 ../lib/p5.o ../lib/p5s.o ../lib/blockdev.o ../lib/memory.o
+ld -o vfs.o -T lscpt.lds init.o main.o -melf_i386 ../lib/p5.o ../lib/p5s.o ../lib/blockdev.o ../lib/memory.o ../lib/memorys.o
 objcopy -O binary -j .init -j .text -j .data -j .bss --set-section-flags .bss=alloc,load,contents vfs.o vfs.mod
 cp vfs.mod ../../../rampak/
